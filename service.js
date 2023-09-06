872a82e1431b0b94b47f8f4c0a409b00c1e5c03a
tohmail1 <tohmail1@users.noreply.github.com>
2023-09-07 02:41:15 +0700

    Update service.js

--- service.js
+++ service.js
@@ -1,16 +1,14 @@
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 4000 }); 
-// สร้าง websockets server ที่ port 4000
-wss.on('connection', function connection(ws) { // สร้าง connection
-  ws.on('message', function incoming(message) {
-   // รอรับ data อะไรก็ตาม ที่มาจาก client แบบตลอดเวลา
-    console.log('received: %s', message);
-    ws.send('I got your: %s', message);
-  });
-ws.on('close', function close() {
-  // จะทำงานเมื่อปิด Connection ในตัวอย่างคือ ปิด Browser
-    console.log('disconnected');
-  });
-ws.send('init message to client');
-  // ส่ง data ไปที่ client เชื่อมกับ websocket server นี้
+import WebSocket from 'ws';
+
+const ws = new WebSocket('ws://www.host.com/path');
+
+ws.on('error', console.error);
+
+ws.on('open', function open() {
+  ws.send('something');
+});
+
+ws.on('message', function message(data) {
+  console.log('received: %s', data);
+  ws.send('received: %s', data);
 });

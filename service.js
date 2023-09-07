d9b673b4b550e6febb7d971ea3594541d0f16a12
tohmail1 <tohmail1@users.noreply.github.com>
2023-09-08 00:42:25 +0700

    Update service.js

--- service.js
+++ service.js
@@ -7,7 +7,8 @@ const wss = new WebSocketServer({ port: 8080 });
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
     console.log('received: %s', data);
+    ws.boardcast(data);
   });
 
-  ws.send('something');
+  ws.send('proxy connected');
 });

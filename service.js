27c19a67ae5d1ca956fe9390d6d97a760d795f67
tohmail1 <tohmail1@users.noreply.github.com>
2023-09-08 00:01:07 +0700

    Update service.js

--- service.js
+++ service.js
@@ -1,14 +1,12 @@
 //import WebSocket from 'ws';
 const WebSocket = require("ws");
-const ws = new WebSocket('ws://www.host.com/path');
+//const ws = new WebSocket('ws://www.host.com/path');
+const wss = new WebSocketServer({ port: 8080 });
 
-ws.on('error', console.error);
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
 
-ws.on('open', function open() {
   ws.send('something');
-});
-
-ws.on('message', function message(data) {
-  console.log('received: %s', data);
-  ws.send('received: %s', data);
 });

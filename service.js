3e19d688faef0d186ebafa5aef3f722b8ec803af
tohmail1 <tohmail1@users.noreply.github.com>
2023-09-13 12:08:39 +0700

    Update service.js

--- service.js
+++ service.js
@@ -4,7 +4,8 @@ import { WebSocketServer } from 'ws';
 //const ws = new WebSocket('ws://www.host.com/path');
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+wss.on('connection', function connection(ws, req) {
+  console.log('connection from: %s', req.socket.remoteAddress);
   ws.on('message', function message(data) {
     console.log('received: %s', data);
     wss.clients.forEach(client => if(ws != client) client.send(data));

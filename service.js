db44047eb2b1aabdb412eb0ce3b20763a62f412a
tohmail1 <tohmail1@users.noreply.github.com>
2023-09-13 12:09:48 +0700

    Update service.js

--- service.js
+++ service.js
@@ -2,14 +2,21 @@
 //const WebSocketServer = require("ws");
 import { WebSocketServer } from 'ws';
 //const ws = new WebSocket('ws://www.host.com/path');
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: 80 });
 
 wss.on('connection', function connection(ws, req) {
   console.log('connection from: %s', req.socket.remoteAddress);
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-    wss.clients.forEach(client => if(ws != client) client.send(data));
-  });
+  ws.on('error', console.error);
 
-  ws.send('proxy connected');
+  ws.on('message', function message(data, isBinary) {
+      wss.clients.forEach(function each(client) {
+          if (client !== ws && client.readyState === WebSocket.OPEN) {
+              console.log('send');
+              //client.send(data, { binary: isBinary });
+              client.send('data from another device.');
+          }
+      });
+  });
+    
+  ws.send('You successfully connected to the websocket 1.');
 });
